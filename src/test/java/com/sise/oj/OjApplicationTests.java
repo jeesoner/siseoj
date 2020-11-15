e8a3b6a96b3f7d16229beabd1a62e369cbc29a37
cijee <saykuray@foxmail.com>
2020-11-16 00:20:00 +0800

    siseoj项目初始化

---
+++ src/test/java/com/sise/oj/OjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sise.oj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
