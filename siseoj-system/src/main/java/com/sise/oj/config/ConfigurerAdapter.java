a913c9cf132c4813dfea0aa92dbd835700547957
cijee <saykuray@foxmail.com>
2020-12-03 11:43:51 +0800

    fix(siseoj-system v1.0): 解决fastjson解析日期无效问题

--- siseoj-system/src/main/java/com/sise/oj/config/ConfigurerAdapter.java
+++ siseoj-system/src/main/java/com/sise/oj/config/ConfigurerAdapter.java
@@ -1,9 +1,15 @@
 package com.sise.oj.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * Spring MVC自定义配置
@@ -43,6 +49,24 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
         registry.addResourceHandler("/avatar/**").addResourceLocations(avatarPath);
         registry.addResourceHandler("/file/**").addResourceLocations(filePath);
         registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //1、定义一个convert转换消息的对象
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        //2、添加fastjson的配置信息
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.DisableCircularReferenceDetect,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteDateUseDateFormat,
+                SerializerFeature.WriteMapNullValue);
+        //4、在convert中添加配置信息
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        //5、将convert添加到converters中，并设置优先级
+        converters.add(0, fastConverter);
     }
 
 }
