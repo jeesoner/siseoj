f9bcfefa28967b447b10ddf47a5b5cc6d84462ec
cijee <saykuray@foxmail.com>
2021-03-08 02:02:46 +0800

    feat: 新增菜单管理接口

---
+++ siseoj-system/src/main/java/com/sise/oj/service/MenuService.java
@@ -0,0 +1,11 @@
+package com.sise.oj.service;
+
+import com.sise.oj.base.BaseService;
+import com.sise.oj.domain.Menu;
+
+/**
+ * @author Cijee
+ * @version 1.0
+ */
+public interface MenuService extends BaseService<Menu> {
+}
---
+++ siseoj-system/src/main/java/com/sise/oj/service/impl/MenuServiceImpl.java
@@ -0,0 +1,15 @@
+package com.sise.oj.service.impl;
+
+import com.sise.oj.base.BaseServiceImpl;
+import com.sise.oj.domain.Menu;
+import com.sise.oj.mapper.MenuMapper;
+import com.sise.oj.service.MenuService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Cijee
+ * @version 1.0
+ */
+@Service
+public class MenuServiceImpl extends BaseServiceImpl<MenuMapper, Menu> implements MenuService {
+}
