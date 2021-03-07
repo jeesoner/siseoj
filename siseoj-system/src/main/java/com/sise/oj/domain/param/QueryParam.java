92104c9d452faf7b4a43bb2af1f3edbb10a8e24e
cijee <saykuray@foxmail.com>
2021-03-08 02:00:39 +0800

    新增前端通用查询类

---
+++ siseoj-system/src/main/java/com/sise/oj/domain/param/QueryParam.java
@@ -0,0 +1,25 @@
+package com.sise.oj.domain.param;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Cijee
+ * @version 1.0
+ */
+@Data
+public class QueryParam {
+
+    private Long id;
+
+    /**
+     * 查询关键字，可能为 username, nickname, name
+     */
+    private String keyword;
+
+    private Boolean enabled;
+
+    private List<Date> createTime;
+}
