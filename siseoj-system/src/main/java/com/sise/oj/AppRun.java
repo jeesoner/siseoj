12701c182ea9024ffca82e880b8af2fadacde669
cijee <saykuray@foxmail.com>
2021-03-20 23:54:20 +0800

    feat(siseoj-system v1.0): 基础框架配置

--- siseoj-system/src/main/java/com/sise/oj/AppRun.java
+++ siseoj-system/src/main/java/com/sise/oj/AppRun.java
@@ -3,6 +3,7 @@ package com.sise.oj;
 import com.sise.oj.annotation.rest.AnonymousGetMapping;
 import com.sise.oj.util.SpringContextHolder;
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
 @EnableAsync
 @RestController
 @Api(hidden = true)
@@ -27,6 +32,15 @@ public class AppRun {
     @Bean
     public SpringContextHolder springContextHolder() {
         return new SpringContextHolder();
+    }
+
+    @Bean(name = "crawlExecutorPool")
+    public ExecutorService crawlExecutorPool() {
+        // 获取Java虚拟机的可用的处理器数，最佳线程个数，处理器数*2。根据实际情况调整
+        int curSystemThreads = Runtime.getRuntime().availableProcessors() * 2;
+        log.info("------------系统可用线程池线程个数：{}------------", curSystemThreads);
+        // 创建线程池
+        return Executors.newFixedThreadPool(curSystemThreads);
     }
 
     /**
