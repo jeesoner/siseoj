344d629a3eb4235d58779fc86587eda43af26c11
cijee <saykuray@foxmail.com>
2021-03-27 01:43:32 +0800

    feat(siseoj-common v1.0): 增加乐观锁的AOP处理

--- siseoj-common/src/main/java/com/sise/oj/config/MybatisPlusConfig.java
+++ siseoj-common/src/main/java/com/sise/oj/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package com.sise.oj.config;
 
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConditionalOnClass(value = {PaginationInterceptor.class})
 public class MybatisPlusConfig {
+
+    //注册乐观锁插件
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor(){
+        return new OptimisticLockerInterceptor();
+    }
 
     // 分页插件
     @Bean
