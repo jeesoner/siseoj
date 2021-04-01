ca5bf3b754a5a1cb459e3e99231f033c6178ca1f
cijee <saykuray@foxmail.com>
2021-04-01 15:37:59 +0800

    feat(siseoj-system v1.0): 增加文件上传功能

---
+++ siseoj-common/src/main/java/com/sise/oj/util/CloseUtils.java
@@ -0,0 +1,30 @@
+package com.sise.oj.util;
+
+import java.io.Closeable;
+
+/**
+ * @author Cijee
+ * @version 1.0
+ */
+public class CloseUtils {
+
+    public static void close(Closeable closeable) {
+        if (null != closeable) {
+            try {
+                closeable.close();
+            } catch (Exception e) {
+                // 静默关闭
+            }
+        }
+    }
+
+    public static void close(AutoCloseable closeable) {
+        if (null != closeable) {
+            try {
+                closeable.close();
+            } catch (Exception e) {
+                // 静默关闭
+            }
+        }
+    }
+}
