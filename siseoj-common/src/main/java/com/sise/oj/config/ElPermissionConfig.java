b4029da62c5c3510e68c96b6d841b4ff136b5fd3
cijee <saykuray@foxmail.com>
2021-04-22 10:09:12 +0800

    style(siseoj-common v1.0): 修改代码格式

--- siseoj-common/src/main/java/com/sise/oj/config/ElPermissionConfig.java
+++ siseoj-common/src/main/java/com/sise/oj/config/ElPermissionConfig.java
@@ -19,7 +19,8 @@ public class ElPermissionConfig {
 
     public Boolean check(String ...permissions) {
         // 获取当前用户的所有权限
-        List<String> elPermissions = SecurityUtils.getCurrentUser().getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+        List<String> elPermissions = SecurityUtils.getCurrentUser().getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
         System.out.println("当前用户的所有权限: " + SecurityUtils.getCurrentUsername() + elPermissions.toString());
         // 判断当前用户的所有权限是否包含接口上定义的权限
         return elPermissions.contains("root") || Arrays.stream(permissions).anyMatch(elPermissions::contains);
